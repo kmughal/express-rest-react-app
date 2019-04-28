dc55cd50165e36c7c955741efe4d216f18b2a96e
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-28 16:57:32 +0100

    Change is auth middleware
    - Instead of throwing error set a isAuth flag

--- server/middlewares/is-auth.js
+++ server/middlewares/is-auth.js
@@ -3,9 +3,11 @@ const jwt = require("jsonwebtoken");
 exports.AuthMiddleware = (req, res, next) => {
 	const authorizationHeader = req.headers.authorization;
 	if (!authorizationHeader) {
-		const noAuthHeaderError = new Error("Auth token not provided");
-		noAuthHeaderError.messages = ["missing auth token"];
-		throw noAuthHeaderError;
+		// const noAuthHeaderError = new Error("Auth token not provided");
+		// noAuthHeaderError.messages = ["missing auth token"];
+		// throw noAuthHeaderError;
+		req.isAuth = false;
+		return next();
 	}
 
 	const token = authorizationHeader.split(" ")[1];
@@ -13,18 +15,20 @@ exports.AuthMiddleware = (req, res, next) => {
 	try {
 		decodedToken = jwt.verify(token, "some-key");
 	} catch (e) {
-		console.log(e);
-		e.statusCode = 500;
-		e.messages = ["Invalid token"];
-		throw e;
+		req.isAuth = false;
+		return next();
 	}
 
 	if (!decodedToken) {
-		const notAuthorizedTokenError = new Error("Not authorized");
-		notAuthorizedTokenError.statusCode = 401;
-		throw notAuthorizedTokenError;
+		// const notAuthorizedTokenError = new Error("Not authorized");
+		// notAuthorizedTokenError.statusCode = 401;
+		// throw notAuthorizedTokenError;
+		req.isAuth = false;
+		return next();
 	}
-
+	req.isAuth = true;
 	req.userid = decodedToken.id;
+ console.log("auth token set")
 	next();
 };
+
