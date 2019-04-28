db161ca76d2e36c9f59ed82f8dd1173781504f46
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-28 16:56:09 +0100

    add post service using graphql

---
+++ client/src/graphql-services/post-service.js
@@ -0,0 +1,121 @@
+const SERVICE_URL = "http://localhost:8000/graphql";
+
+export class PostService {
+	async getAllPosts() {
+		const query = {
+			query: `{
+			getPosts{
+				title
+				content
+				image,
+				_id
+			}
+		}`
+		};
+		const options = {
+			method: "POST",
+			body: JSON.stringify(query),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + localStorage.getItem("token")
+			}
+		};
+
+		const response = await fetch(SERVICE_URL, options);
+		const data = await response.json();
+		console.log(data);
+		return data.data.getPosts;
+	}
+
+	deletePost(id) {
+		const data = new FormData();
+		data.append("id", id);
+		return fetch(SERVICE_URL, {
+			method: "DELETE",
+			body: data,
+			headers: {
+				Authorization: "Bearer " + localStorage.getItem("token")
+			}
+		});
+	}
+
+	updatePost(id, title, content, image) {
+		const data = new FormData();
+		data.append("id", id);
+		data.append("title", title);
+		data.append("content", content);
+		data.append("image", image);
+
+		return fetch(SERVICE_URL, {
+			body: data,
+			method: "PUT",
+			headers: {
+				Authorization: "Bearer " + localStorage.getItem("token")
+			}
+		});
+	}
+	uploadImage(image) {
+		const formData = new FormData();
+		formData.append("image", image);
+		return fetch("http://localhost:8000/upload-image", {
+			method: "PUT",
+			body: formData,
+			headers: {
+				Authorization: "Bearer " + localStorage.getItem("token")
+			}
+		});
+	}
+
+	createPost(title, content, image) {
+		return this.uploadImage(image)
+			.then(res => {
+				window.d = res;
+				return res.json();
+			})
+			.then(data => {
+				return data.data.filePath;
+			})
+
+			.then(imagePath => {
+				const query = {
+					query: `
+				mutation {
+					createPost(title:"${title}" , content:"${content}", imageUrl : "${imagePath}") {
+						title,
+						content,
+						image,
+						_id
+					}
+				}
+				`
+				};
+				return fetch(SERVICE_URL, {
+					method: "POST",
+					headers: {
+						"content-type": "application/json",
+						Authorization: "Bearer " + localStorage.getItem("token")
+					},
+					body: JSON.stringify(query)
+				});
+			})
+			;
+
+		// const data = new FormData();
+		// data.append("title", title);
+		// data.append("content", content);
+		// data.append("image", image);
+
+		// return fetch(SERVICE_URL, {
+		// 	body: data,
+		// 	method: "POST",
+		// 	headers: {
+		// 		Authorization: "Bearer " + localStorage.getItem("token")
+		// 	}
+		// });
+		// return fetch(SERVICE_URL, {
+		// 	body: JSON.stringify({ title, content }),
+		// 	method: "POST",
+		// 	headers: { "Content-Type": "application/json" }
+		// });
+	}
+}
