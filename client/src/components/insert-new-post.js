710d1cb67f36fce5e9142a0f08322546d038ee0b
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-28 16:55:28 +0100

    Use graphql service and update code inline with response

--- client/src/components/insert-new-post.js
+++ client/src/components/insert-new-post.js
@@ -1,15 +1,17 @@
 import * as React from "react";
-import { PostService } from "../services/post-service";
+import { PostService } from "../graphql-services/post-service";
 
 const buildErrorStringMarkup = data => {
+	if (!data.errors) return "added"
+	const errors = data.errors[0].messages;
 	const errorHtml = ['<ul class="alert--danger">'];
-	data.forEach(m => errorHtml.push(`<li>${m}</li>`));
+	errors.forEach(m => errorHtml.push(`<li>${m}</li>`));
 	errorHtml.push("</ul>");
 	return errorHtml.join("");
 };
 
 const updateSubmitResultMarkup = text =>
-	(document.getElementById("submit--results").innerHTML = text);
+	document.getElementById("submit--results").innerHTML = text;
 
 	
 const sendPost = () => {
@@ -18,16 +20,19 @@ const sendPost = () => {
 		image = document.getElementById("image").files[0];
 
 	const service = new PostService();
+	
 	service.createPost(title, content,image).then(response => {
-		const { status } = response;
-		if (status !== 201) {
+		//const { status } = response;
+		//if (status !== 201)
+		 {
 			response
 				.json()
 				.then(buildErrorStringMarkup)
 				.then(updateSubmitResultMarkup);
-		} else {
-			updateSubmitResultMarkup("Post added!");
-		}
+		} 
+		//else {
+		//	updateSubmitResultMarkup("Post added!");
+		//}
 	});
 };
 
