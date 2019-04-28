fd733ce642f03d79bc67b0317d4c607a3de40713
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-28 18:35:11 +0100

    use variables instaed of string interpolation in graphql

--- client/src/graphql-services/auth-service.js
+++ client/src/graphql-services/auth-service.js
@@ -2,44 +2,48 @@ const SERVICE_URL = "http://localhost:8000/graphql";
 
 export class AuthService {
 	signup(name, email, password) {
-    const query = { query : `
-    mutation {
-      createUser(registerUserRequest:{name :"${name}" , email:"${email}",  password :"${password}"}){
+		const query = {
+			query: `
+    mutation SetupUser($name :String!,$email:String!,$password:String!) {
+      createUser(registerUserRequest:{name :$name , email:$email,  password :$password}){
         _id
         name
         password
       }
     }
-    `}
+    `,
+			variables: { name: name, email: email, password: password }
+		};
 
 		return fetch(SERVICE_URL, {
 			method: "POST",
-      body: JSON.stringify(query),
-      headers : {
-				"content-type" : "application/json",
+			body: JSON.stringify(query),
+			headers: {
+				"content-type": "application/json",
 				Authorization: "Bearer " + localStorage.getItem("token")
-      }
+			}
 		});
 	}
 
-
 	signin(email, password) {
-		const query = JSON.stringify({ query : `
+		const query = JSON.stringify({
+			query: `
 		{
 			signIn(email:"${email}",password:"${password}") {
 				token
 				name
 			}
 		}
-		`});
+		`
+		});
 
 		return fetch(`${SERVICE_URL}`, {
 			method: "POST",
 			body: query,
-			headers : {
-				"content-type" : "application/json",
+			headers: {
+				"content-type": "application/json",
 				Authorization: "Bearer " + localStorage.getItem("token")
-      }
+			}
 		});
 	}
 }
