ec1c169c8b412e2cdb03f9de8fa836359c6ee68b
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-05-06 17:08:14 +0100

    add an override to mongoose exec

---
+++ server/commons/moogose-exec.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { createRedisClient } = require("../infrastructure/redis-client");
+
+const exec = mongoose.Query.prototype.exec;
+const redisClient = createRedisClient();
+
+mongoose.Query.prototype.exec = async function() {
+	// 1 create a unique key
+	const collectionname = this.mongooseCollection.name;
+	const queryOption = this.getQuery();
+	const key = JSON.stringify(
+		Object.assign({}, queryOption, { collection: collectionname })
+	);
+
+	// 2 check the value in redis if present then return from radis
+	const cacheValueString = await redisClient.get(key);
+	if (cacheValueString) {
+		console.log("serving from cache");
+		const cacheValue = JSON.parse(cacheValueString);
+
+		return Array.isArray(cacheValue)
+			? cacheValue.map(d => this.model(d))
+			: this.model(cacheValue);
+	}
+	// 3 fall back to original version, run the query and store in cache.
+	const result = await exec.apply(this, arguments);
+	redisClient.set(key, JSON.stringify(result));
+	return result;
+};
