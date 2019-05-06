25eb728d8ddca25a80325cd2bc813011bc210dba
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-05-06 17:50:58 +0100

    promisify hset and hget during client creation for redi

--- server/infrastructure/redis-client.js
+++ server/infrastructure/redis-client.js
@@ -4,13 +4,14 @@ const { promisify } = require("util");
 let client = null;
 
 const createRedisClient = () => {
-	
 	if (client === null) {
 		client = redis.createClient(url);
-		client.on('error', console.log);
+		client.on("error", console.log);
 	}
 	client.set = promisify(client.set);
 	client.get = promisify(client.get);
+	client.hset = promisify(client.hset);
+	client.hget = promisify(client.hget);
 
 	return client;
 };
