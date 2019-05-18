65e29f9b417013f9ad8bf45574a36448f89963a7
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-05-18 10:22:21 +0100

    add proxy for custom page object

---
+++ client/tests/proxies/index.js
@@ -0,0 +1,57 @@
+const puppeteer = require("puppeteer");
+
+exports.CustomPage = class CustomPage {
+	constructor(page) {
+		this.page = page;
+	}
+
+	static async buildPage() {
+		const newBrowser = await puppeteer.launch({ headless: true });
+		const newPage = await newBrowser.newPage();
+		const self = new CustomPage(newPage);
+
+		return new Proxy(self, {
+			get: function(target, property) {
+				return newBrowser[property] || target[property] || newPage[property];
+			}
+		});
+	}
+
+	async getSignInButtonText() {
+		return await this.page.$eval(
+			".top-action a:nth-child(2)",
+			el => el.innerHTML
+		);
+	}
+
+	async redirectToSignInPageAndGetUrl() {
+		await this.page.click(".top-action a:nth-child(2)");
+		const url = await this.page.url();
+		return url;
+	}
+
+	async login(email, password) {
+		await this.page.click(".top-action a");
+
+		await this.page.waitFor("#email");
+		await this.page.type("#email", email);
+		await this.page.type("#password", password);
+		await this.page.click(".action-buttons button");
+		await this.page.waitFor(".all-posts");
+
+		const url = await this.page.url();
+		return url;
+	}
+
+	async signout(email, password) {
+		await this.page.click(".top-action a");
+		await this.page.type("#email", email);
+		await this.page.type("#password", password);
+		await this.page.click(".action-buttons button");
+		await this.page.waitFor(".all-posts");
+		await this.page.click(".top-action a");
+		await this.page.waitFor(".logout-message");
+		const url = await this.page.url();
+		return url;
+	}
+};
